0714d2e61aede5f0e83558cd1c74d45aeb9ccd3e
themonster3004 <nqd5758@gmail.com>
2023-06-29 20:49:33 +0700

    [Quiz] Currency exchange

---
+++ CurrencyExchange/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Program;
+
+class Program {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter value to exchange (1 USD = 23000 VND): ");
+        double valueUSD = Convert.ToDouble(Console.ReadLine());
+        double valueVND = valueUSD * 23000;
+        Console.WriteLine(valueUSD + " USD = " + valueVND + " VND.");
+    }
+}
