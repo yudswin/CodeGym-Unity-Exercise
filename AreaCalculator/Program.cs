51b79949b04fe96ba3b4b9640c45392de5c4ef2a
themonster3004 <nqd5758@gmail.com>
2023-06-29 20:23:17 +0700

    [Quiz] Area calculator

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the length of Rectangle: ");
+        double l = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the width of Rectangele: ");
+        double w = Convert.ToDouble(Console.ReadLine());
+        double area = w * l;
+        Console.WriteLine("Area of Rectangle: " + area);
+    }
+}
