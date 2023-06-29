a7dac1fa9cdbe8d6f2d9f26a82a88643b67da074
themonster3004 <nqd5758@gmail.com>
2023-06-29 20:35:17 +0700

    [Quiz] Username display

---
+++ UserName/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Program;
+
+class Program {
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter username: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Hello " + username + "!");
+    }
+}
