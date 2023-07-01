81fcb2c80e8f3030cbb681250eec85b261adf08f
themonster3004 <nqd5758@gmail.com>
2023-07-01 21:22:29 +0700

    [Quiz] Linear Equation resolver

---
+++ LinearEquationResolver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearEquationResolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("The solution is: {0}!", solution);
+            }
+
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("The solution is all x!");
+                }
+                else
+                {
+                    Console.Write("Nod solution!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
