3428f216312cf322b5dded957d86f616d7ef962f
themonster3004 <nqd5758@gmail.com>
2023-07-02 00:38:53 +0700

    [Quiz] BMI Calculator

---
+++ BMICaculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICaculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height;
+            double weight;
+
+            Console.WriteLine("Please enter a height (meter)");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your weight (kg)");
+            weight = float.Parse(Console.ReadLine());
+
+            double bmi = weight / Math.Pow(height, 2);
+            bmi = Math.Round(bmi, 1);
+
+            Console.Write("BMI: " + bmi);
+
+            if (bmi < 18)
+                Console.WriteLine(" Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine(" Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine(" Overweight");
+            else
+                Console.WriteLine(" Obese");
+            Console.ReadKey();
+        }
+    }
+}
