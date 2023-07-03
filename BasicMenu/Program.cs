af2cd4dd690cd4fb7c96252c3812e083bb0889b3
themonster3004 <nqd5758@gmail.com>
2023-07-03 14:05:19 +0700

    [Quiz] Shape Menu

--- BasicMenu/Program.cs
+++ BasicMenu/Program.cs
@@ -11,13 +11,9 @@ namespace BasicMenu
         static void Main(string[] args)
         {
 
-            DrawSquare(5);
-            Console.WriteLine();
-            DrawRectangle(7, 9);
-            Console.WriteLine();
-            DrawTriangle(4, 5);
-
         UserInput:
+            Console.WriteLine();
+            Console.WriteLine("------------------");
             Console.WriteLine("Menu\r\n" +
                 "1. Draw the triangle\r\n" +
                 "2. Draw the square\r\n" +
@@ -29,31 +25,55 @@ namespace BasicMenu
             {
                 case ConsoleKey.D1:
                     Console.WriteLine();
-                    Console.WriteLine("Enter Draw the triangle");
-                    
+                    Console.WriteLine("Chose type draw the triangle\r\n" +
+                    "1. top-left\r\n" +
+                    "2. top-right\r\n" +
+                    "3. botton-left\r\n" +
+                    "4. botton-right\r\n" +
+                    "0. Exit\r\n");
+                    Console.Write("Enter your choice:");
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.D1:
+                            Console.WriteLine();
+                            Console.WriteLine("Draw the top-left triangle");
+                            DrawTriangle(5, 1);
+                            break;
+                        case ConsoleKey.D2:
+                            Console.WriteLine();
+                            Console.WriteLine("Draw the top-right triangle");
+                            DrawTriangle(5, 2);
+                            break;
+                        case ConsoleKey.D3:
+                            Console.WriteLine();
+                            Console.WriteLine("Draw the bottom-left triangle");
+                            DrawTriangle(5, 3);
+                            break;
+                        case ConsoleKey.D4:
+                            Console.WriteLine();
+                            Console.WriteLine("Draw the bottom-right triangle");
+                            DrawTriangle(5, 4);
+                            break;
+                        case ConsoleKey.D0:
+                            Environment.Exit(0);
+                            break;
+                    }
                     break;
                 case ConsoleKey.D2:
                     Console.WriteLine();
                     Console.WriteLine("Draw the square");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
+                    DrawSquare(5);
                     break;
                 case ConsoleKey.D3:
                     Console.WriteLine();
                     Console.WriteLine("Draw the rectangle");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
-                    Console.WriteLine("* * * * * *");
+                    DrawRectangle(3, 5);
                     break;
                 case ConsoleKey.D0:
                     Environment.Exit(0);
                     break;
                 default:
-                    Console.WriteLine("No choice!");
+                    Console.WriteLine("\nNo choice!");
                     break;
             }
 
@@ -61,15 +81,64 @@ namespace BasicMenu
             if (Console.ReadKey().Key == ConsoleKey.C) goto UserInput;
         }
 
-        static void DrawTriangle(int heigth, int length)
+        static void DrawTriangle(int side, int type)
         {
-            for (int i = 0;i < heigth; i++)
+            switch (type)
             {
-                for (int j = 0; j < length; j++)
-                {
-                    Console.Write("* ");
-                }
-                Console.WriteLine();
+                case 1: //top-left
+                    for (int i = 0; i < side; i++)
+                    {
+                        for (int j = 0; j < side - i; j++)
+                        {
+                            Console.Write("* ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2: //top-right
+                    
+                    for (int i = 0; i < side; i++)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            Console.Write("  ");
+                        }
+
+                        for (int j = 0; j < side - i; j++)
+                        {
+                            Console.Write("* ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    break;
+                case 3: //bottom-left
+                    for (int i = 0; i < side + 1; i++)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 4: //bottom-right
+                    for (int i = 0; i < side; i++)
+                    {
+                        for (int j = 0; j < side - i - 1; j++)
+                        {
+                            Console.Write("  ");
+                        }
+
+                        for (int j = 0; j <= i; j++)
+                        {
+                            Console.Write("* ");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    break;
             }
 
         }
