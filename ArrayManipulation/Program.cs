90eecba94ee7154f01383e7817dea5ac54339560
themonster3004 <nqd5758@gmail.com>
2023-07-04 15:34:26 +0700

    [Quiz] Array Manipulation

---
+++ ArrayManipulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+
+            int total = 0;
+            foreach (int num in numbers)
+            {
+                total += num;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
