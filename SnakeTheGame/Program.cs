5876be3139fe48aa098be453ff539fdcacf449b3
themonster3004 <nqd5758@gmail.com>
2023-07-11 18:44:22 +0700

    update Game
    update code, clean, and add comment

--- SnakeTheGame/Program.cs
+++ SnakeTheGame/Program.cs
@@ -2,89 +2,102 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+//sources : https://youtu.be/_GmjXi1kcHA
 
 namespace SnakeTheGame
 {
     internal class Snake
     {
-        int Height = 20;
-        int Width = 30;
-
-        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
-        char key = 'W';
-
+        //Board
+        int Height = 25;
+        int Width = 50;
+        
+        //Snake
+        int parts = 3;
         int[] X = new int[50];
         int[] Y = new int[50];
 
+        //Fruit
         int fruitX;
         int fruitY;
-        int parts = 3;
-
         Random rnd = new Random();
 
+
+        // [CONSTRUCTOR]
         Snake()
         {
+            //Snake start location
             X[0] = 5;
             Y[0] = 5;
-            Console.CursorVisible = false;
+
+            //Fruit start location
             fruitX = rnd.Next(2, (Width - 2));
             fruitY = rnd.Next(2, (Height - 2));
         }
 
-        public void WriteBoard()
+        //[DEBUGGING]
+        private void SnakeLocation() // Update Snake location
         {
-            Console.Clear();
+            Console.SetCursorPosition(Width + 20, 0);
+            Console.Write("X: {0} | Y: {1}", X[0], Y[0]);
+        }
 
-            for (int i = 1; i <= (Width + 2); i++)
+        //[DRAW METHOD]
+        private void DrawBorder()
+        {
+            SnakeLocation();
+            for (int i = 0; i <= Height; i += Height) 
             {
-                Console.SetCursorPosition(i, 1);
-                Console.Write("-");
+                for (int j = 0;j <= Width; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write("#");
+                    }
+                }
             }
 
-            for (int i = 1; i <= (Width + 2); i++)
+            for (int i = 0;i <= Width; i += Width)
             {
-                Console.SetCursorPosition(i, (Height + 2));
-                Console.Write("-");
-            }
-
-            for (int i = 1; i <= (Height + 1); i++)
-            {
-                Console.SetCursorPosition(1, i);
-                Console.Write("|");
-            }
-
-            for (int i = 1; i <= (Height + 1); i++)
-            {
-                Console.SetCursorPosition((Width + 2), i);
-                Console.Write("|");
+                for (int j = 0; j <= Height; j++)
+                {
+                    Console.SetCursorPosition(i, j);
+                    Console.Write("#");
+                }
             }
         }
 
-        public void Input()
-        {
-            if (Console.KeyAvailable)
-            {
-                keyInfo = Console.ReadKey(true);
-                key = keyInfo.KeyChar;
-            }
-        }
-
-        public void WritePoint(int x, int y)
+        private void DrawSnake(int x, int y)
         {
             Console.SetCursorPosition(x, y);
-            Console.Write("#");
+            Console.Write("0");
         }
 
-        public void Logic()
+        private void DrawFruit(int x, int y)
         {
+            Console.SetCursorPosition(x, y);
+            Console.Write("1");
+        }
+
+        //[LOGIC AND MANAGER]
+        private void Logic()
+        {
+            //Fruit hit checking
             if (X[0] == fruitX)
             {
                 if (Y[0] == fruitY)
                 {
-                    parts++;
+                    parts++; //Snake grown
                     fruitX = rnd.Next(2, (Width - 2));
                     fruitY = rnd.Next(2, (Height - 2));
                 }
@@ -96,41 +109,62 @@ namespace SnakeTheGame
                 Y[i - 1] = Y[i - 2];
             }
 
+ 
+            //Draw Snake
+            for (int i = 0; i <= (parts - 1); i++)
+            {
+                DrawSnake(X[i], Y[i]);
+            }
+            DrawFruit(fruitX, fruitY);
+            Thread.Sleep(100);
+        }
+
+        // [CONTROLLER]
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        ConsoleKey key = ConsoleKey.W;
+
+        private void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.Key;
+            }
+
             switch (key)
             {
-                case 'w':
+                case ConsoleKey.UpArrow:
                     Y[0]--;
                     break;
-                case 's':
+                case ConsoleKey.DownArrow:
                     Y[0]++;
                     break;
-                case 'd':
+                case ConsoleKey.RightArrow:
                     X[0]++;
                     break;
-                case 'a':
+                case ConsoleKey.LeftArrow:
                     X[0]--;
                     break;
             }
+        }
 
-            for (int i = 0; i <= (parts - 1); i++)
+        // [GAME RENDERING]
+        public void Play()
+        {
+            Console.CursorVisible = false;
+            while (true)
             {
-                WritePoint(X[i], Y[i]);
-                WritePoint(fruitX, fruitY);
+                Console.Clear();
+                DrawBorder();
+                Input();
+                Logic();
             }
-
-            Thread.Sleep(100);
         }
 
         static void Main(string[] args)
         {
             Snake snake = new Snake();
-
-            while (true)
-            {
-                snake.WriteBoard();
-                snake.Input();
-                snake.Logic();
-            }
+            snake.Play();            
         }
     }
 }
