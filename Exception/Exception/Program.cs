8e52815305a638e385f34646364f02f47871bd3f
themonster3004 <nqd5758@gmail.com>
2023-07-22 21:08:48 +0700

    [Quiz] Exception

---
+++ Exception/Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6]; //out of range
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
