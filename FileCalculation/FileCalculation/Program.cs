95f71d45b7837c47c9b22216d6f4d7a7b45e6f4e
themonster3004 <nqd5758@gmail.com>
2023-07-29 17:03:07 +0700

    [Quiz] File Sum Calculation

---
+++ FileCalculation/FileCalculation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input file path");
+            string path = Console.ReadLine();
+            Console.WriteLine("File path: " + path);
+            ReadTextFileExample example = new ReadTextFileExample();
+            example.ReadTextFile(path);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileCalculation/FileCalculation/ReadTextFileExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCalculation
+{
+    public class ReadTextFileExample
+    {
+        public void ReadTextFile(string filePath)
+        {
+            {
+                try
+                {
+                    FileInfo file = new FileInfo(filePath);
+                    if (!file.Exists)
+                    {
+                        throw new FileNotFoundException();
+                    }
+
+                    StreamReader reader = new StreamReader(filePath);
+                    string line = "";
+                    int sum = 0;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        sum += Int32.Parse(line);
+                    }
+                    reader.Close();
+                    Console.WriteLine("Total: " + sum);
+                }
+                catch (System.Exception)
+                {
+                    Console.Error.WriteLine("File not found or invalid content");
+                }
+            }
+        }
+    }
+}
