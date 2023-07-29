bb7d177edd2e1438130fdc2d591edd9096cd8318
themonster3004 <nqd5758@gmail.com>
2023-07-29 17:33:28 +0700

    [Quiz] Read and Write File XML

---
+++ FileXMLExample/FileXMLExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileXMLExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWriten = ReadWriteFileXML.WriteToFile();
+            if (isWriten)
+            {
+                ReadWriteFileXML.ReadFromFile();
+            }
+            else
+            {
+                System.Console.WriteLine("Write data to file occur an error. Please try again !");
+            }
+
+        }
+    }
+}
